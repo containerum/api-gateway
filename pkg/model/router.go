7b6234e3d7f35140386095c9f306effd6d3cef46
Konstantin <kfeofantov@yandex.ru>
2017-10-04 20:42:38 +0300

    add proxy router

--- pkg/model/router.go
+++ pkg/model/router.go
@@ -1,4 +1,16 @@
 package model
 
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+//Router is struct for keep user proxy task
 type Router struct {
+	ID    uuid.UUID `sql:"type:uuid"`
+	Group string
+
+	Active  bool
+	Created time.Time
 }
