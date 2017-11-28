a122fb334d78dbb556f703049f1ac8a1327daf9d
Konstantin <kfeofantov@yandex.ru>
2017-11-28 20:30:31 +0300

    add status handler

--- pkg/router/defaults.go
+++ pkg/router/defaults.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"net/http"
+	"os"
 )
 
 func noRouteHandler() http.HandlerFunc {
@@ -14,5 +15,17 @@ func noRouteHandler() http.HandlerFunc {
 func rootRouteHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello! I am API Gateway."))
+	}
+}
+
+// TODO: make checker
+func statusHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name, err := os.Hostname()
+		if err != nil {
+			// TODO: return error
+			panic(err)
+		}
+		w.Write([]byte(name))
 	}
 }
