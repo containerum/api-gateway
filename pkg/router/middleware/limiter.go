735e8e0b36234e7fa4388d1fa2ff59af6560d56b
Konstantin <kfeofantov@yandex.ru>
2017-12-01 20:58:51 +0300

    add vendor redis, auth

---
+++ pkg/router/middleware/limiter.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"net"
+	"net/http"
+
+	rate "git.containerum.net/ch/ratelimiter"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+//Rate limitate reuests per second
+func Rate(limiter **rate.PerIPLimiter) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if *limiter != nil {
+				host, _, _ := net.SplitHostPort(r.RemoteAddr)
+				ok, err := (*limiter).Limit(host)
+				log.WithFields(log.Fields{
+					"Host": host,
+					"OK":   ok,
+					"Err":  err,
+				}).Debug("rate")
+				if !ok {
+					w.WriteHeader(http.StatusTooManyRequests)
+					return
+				}
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
