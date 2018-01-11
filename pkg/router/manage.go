d586cabd3abf2bee1d3f06f1cda3ada40caa6137
Konstantin Feofantov <kfeofantov@gmail.com>
2018-01-11 17:58:54 +0300

    fix Null answer, add group handler

--- pkg/router/manage.go
+++ pkg/router/manage.go
@@ -13,11 +13,15 @@ func CreateManageRouter(router *Router) http.Handler {
 	r := chi.NewRouter()
 	// Router handlers
 	m := manage.NewManager(router.store)
+	/* Listeners */
 	r.Get("/route", m.GetAllRouter())
 	r.Post("/route", m.CreateRouter())
 	r.Get("/route/{id}", m.GetRouter())
 	r.Put("/route/{id}", m.UpdateRouter())
 	r.Delete("/route/{id}", m.DeleteRouter())
+	/* Groups */
+	r.Get("/group", m.GetAllGroup())
+
 	return r
 }
 
