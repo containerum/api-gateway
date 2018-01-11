c01e364d48a2aa1e7b2f4e1ea5446e5238b7e1e5
Konstantin Feofantov <kfeofantov@gmail.com>
2018-01-11 18:49:43 +0300

    bug fix, get groups return array

--- pkg/store/datastore/group.go
+++ pkg/store/datastore/group.go
@@ -1,13 +1,23 @@
 package datastore
 
 import (
+	"errors"
+
 	"git.containerum.net/ch/api-gateway/pkg/model"
+
+	log "github.com/Sirupsen/logrus"
 )
 
-func (d *datastore) GetGroupList(g *model.Group) (*model.Group, error) {
-	var group model.Group
-	err := d.Where(g).Find(&group).Error
-	return &group, err
+func (d *datastore) GetGroupList(g *model.Group) (*[]model.Group, error) {
+	reqName := "GetGroupList call"
+	var groups []model.Group
+	err := d.Where(g).Find(&groups).Error
+	if err != nil {
+		log.WithError(err).Error(reqName)
+		return nil, errors.New("")
+	}
+	log.Debug(reqName)
+	return &groups, nil
 }
 
 func (d *datastore) CreateGroup(g *model.Group) (*model.Group, error) {
