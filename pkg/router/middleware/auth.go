70156039d6d801a97f20b554f38bc5764e26c64d
Konstantin Feofantov <kfeofantov@gmail.com>
2018-01-11 22:34:47 +0300

    temp

--- pkg/router/middleware/auth.go
+++ pkg/router/middleware/auth.go
@@ -11,6 +11,9 @@ func CheckAuthToken(authClient *auth.AuthClient) func(http.Handler) http.Handler
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Print("CHECK TOKEN")
+			next.ServeHTTP(w, r)
+			// w.Write([]byte("OK"))
+			// w.WriteHeader(http.StatusOK)
 		})
 	}
 }
