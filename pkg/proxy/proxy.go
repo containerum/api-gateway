3184b39a669e6c224a99eaf4daabba9c623db585
Konstantin Feofantov <kfeofantov@gmail.com>
2018-01-16 20:49:58 +0300

    upd check stripPart

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -33,11 +33,9 @@ func createDirector(target *model.Listener) func(r *http.Request) {
 		r.URL.Host = targetURL.Host
 		r.URL.Path = singleJoiningSlash(targetURL.Path, r.URL.Path)
 
-		if target.StripPath != nil {
-			if *target.StripPath {
-				strPath := stripPath(target.ListenPath, r.URL.Path)
-				r.URL.Path = singleJoiningSlash(targetURL.Path, strPath)
-			}
+		if target.StripPath {
+			strPath := stripPath(target.ListenPath, r.URL.Path)
+			r.URL.Path = singleJoiningSlash(targetURL.Path, strPath)
 		}
 	}
 }
