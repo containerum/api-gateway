2fe50540b463e1629f78ce7cf30429651725891a
Konstantin Feofantov <kfeofantov@gmail.com>
2018-01-18 20:12:43 +0300

    add auth check

--- pkg/router/middleware/auth.go
+++ pkg/router/middleware/auth.go
@@ -33,24 +33,31 @@ func CheckAuthToken(authClient *auth.AuthClient) func(http.Handler) http.Handler
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			accessToken := r.Header.Get(authorizationHeader)
+			log.WithFields(log.Fields{
+				"useragent": w.Header().Get(userAgentXHeaderName),
+				"finger":    w.Header().Get(userClientXHeaderName),
+				"user":      &common.UUID{Value: "396b8880-02f5-4ca0-832e-90c2b7bb543c"},
+				"ip":        w.Header().Get(userIPXHeaderName),
+				"role":      auth.Role_USER,
+			}).Debug("Create token info")
 
-			res, err := (*authClient).CreateToken(context.Background(), &auth.CreateTokenRequest{
-				UserAgent:   r.Header.Get(userAgentXHeaderName),
-				Fingerprint: r.Header.Get(userClientXHeaderName),
-				UserId:      &common.UUID{Value: "396b8880-02f5-4ca0-832e-90c2b7bb543c"},
-				UserIp:      r.Header.Get(userIPXHeaderName),
-				UserRole:    auth.Role_USER,
-			})
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.WithField("Token", res.AccessToken).Info("Create token success")
+			// res, err := (*authClient).CreateToken(context.Background(), &auth.CreateTokenRequest{
+			// 	UserAgent:   w.Header().Get(userAgentXHeaderName),
+			// 	Fingerprint: w.Header().Get(userClientXHeaderName),
+			// 	UserId:      &common.UUID{Value: "396b8880-02f5-4ca0-832e-90c2b7bb543c"},
+			// 	UserIp:      w.Header().Get(userIPXHeaderName),
+			// 	UserRole:    auth.Role_USER,
+			// })
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+			// log.WithField("Token", res.AccessToken).Info("Create token success")
 
 			token, err := (*authClient).CheckToken(context.Background(), &auth.CheckTokenRequest{
 				AccessToken: accessToken,
-				UserAgent:   r.Header.Get(userAgentXHeaderName),
-				FingerPrint: r.Header.Get(userClientXHeaderName),
-				UserIp:      r.Header.Get(userIPXHeaderName),
+				UserAgent:   w.Header().Get(userAgentXHeaderName),
+				FingerPrint: w.Header().Get(userClientXHeaderName),
+				UserIp:      w.Header().Get(userIPXHeaderName),
 			})
 			if err != nil {
 				w.WriteHeader(http.StatusUnauthorized)
--- pkg/router/middleware/headers.go
+++ pkg/router/middleware/headers.go
@@ -115,3 +115,14 @@ func CheckRequiredXHeaders(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// const (
+// 	UserIDHeader      = "X-User-ID"
+// 	SessionIDHeader   = "X-Session-ID"
+// 	FingerprintHeader = "X-User-Client"
+// 	UserAgentHeader   = "X-User-Agent"
+// 	ClientIPHeader    = "X-Client-IP"
+// 	TokenIDHeader     = "X-Token-ID"
+// 	PartTokenIDHeader = "X-User-Part-Token-ID"
+// 	UserRoleHeader    = "X-User-Role"
+// )
